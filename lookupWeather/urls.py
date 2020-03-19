61538cdf0f850b51c3be1f4604613f47223d3beb
Babu Ajay <babuajaykumar@gmail.com>
2020-03-19 11:07:06 +0530

    added comment on urls.py file

--- lookupWeather/urls.py
+++ lookupWeather/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+#Thsi is my urls.py file for views
 from django.urls import path, include
 
 from lookupWeather import views
